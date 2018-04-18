1e0fd95285b4d597169f654e3f22501062d6c5c7
Danilo Novaes <danilo.novaes@live.com>
2018-04-17 21:51:17 -0300

    Conectado com o banco

--- app/routes/catalogo.js
+++ app/routes/catalogo.js
@@ -1,5 +1,27 @@
 module.exports = function (app) {
     app.get('/catalogo', function (req, res) {
-        res.render('roldanas/catalogo');
+
+
+        var mysql = require('mysql');
+        var connection = mysql.createConnection({
+            host: 'roldana.chtd4y9zgelt.us-east-1.rds.amazonaws.com',
+            user: 'root',
+            password: 'Pa$$4eit',
+            database: 'roldanaDB'
+        })
+
+
+        connection.query('SELECT * FROM roldonas', function (error, results) {
+            if (error) throw error;
+            // console.log('Conexão com o banco falhou', results);
+            res.render('roldanas/catalogo', {
+                resultsHTML: results
+            })
+
+        });
+
+        connection.end();
+
+        // res.render('roldanas/catalogo');
     });
 }
